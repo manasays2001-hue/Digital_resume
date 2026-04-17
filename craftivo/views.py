e91495ae1bb12fcfd64f00cce7b7b549aeb3f886
unknown <manasays2001@gmail.com>
2026-04-17 17:06:10 +0530

    added SMPT

--- craftivo/views.py
+++ craftivo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.core.mail import send_mail
 import random
 
@@ -11,51 +11,23 @@ def home(request):
    
 
 def display(request):
-    if request.method == "POST":
-        email = request.POST.get('email')   # get email from form
-
-        otp = random.randint(1000, 9999)    # better OTP range
-
-        # store OTP in session
-        request.session['generated_otp'] = str(otp)
-        request.session['email'] = email
-        del request.session['generated_otp']
-
-        # send OTP to email
-        send_mail(
-            subject="Your OTP Verification Code",
-            message=f"Your OTP is: {otp}",
-            from_email="your_email@gmail.com",
-            recipient_list=[email],
-            fail_silently=False
-        )
-
-        return render(request, "display.html")
-
-    return render(request, "display.html")
-          
-def verify_otp(request):
-    if request.method == "POST":
-        user_otp = request.POST.get('verify_otp')
-        generated_otp = request.session.get('generated_otp')
-
-        if user_otp == generated_otp:
-            return render(request, "index.html", {"success": "OTP Verified"})
-        else:
-            return render(request, "display.html", {"error": "Invalid OTP"})
-     
-def contact(request):
-      if request.method == "POST":
+     if request.method == "POST":
          name=request.POST['name'] 
          email=request.POST['email']
          subject=request.POST['subject']
          message=request.POST['message']
+        
+         otp = random.randint(1000, 9999)    # better OTP range
+         print(str(otp))
+         request.session['test'] = otp
+         request.session['contact_email'] = email
+
          if email:
             send_mail(
                subject="Thank you for contacting the page",
                message=f""" 
-               Hi  {name} , this is an welcome message from manasa ,we'll get what your looking from me as soon as possible, thank you for contacting us.""",
-               from_email="mailtomanasays2001@gmail.com",
+               Hi  {name} , this is an welcome message from manasa ,we'll get what your looking from me as soon as possible, thank you for contacting us.your OTP is {otp}.""",
+               from_email="manasays2001@gmail.com",
                recipient_list=[email],
                fail_silently=True
             )
@@ -63,7 +35,45 @@ def contact(request):
                subject="New user enquiry",
                message=f""" 
                Hi  {name} , enquired about {subject} with the message {message} and the email is {email}.""",
-               from_email="mailtocontact@gmail.com",
+               from_email="shreyasms6@gmail.com",
+               recipient_list=[email],
+               fail_silently=True
+            )
+            print("mail sent successfully")
+            return redirect(request,"verify_otp")
+          
+def verify_otp(request):
+    if request.method == "POST":
+        otp = request.POST.get('otp')
+        generated_otp = request.session.get('otp')
+
+        if otp == generated_otp:
+            return render(request, "index.html", {"success": "OTP Verified"})
+        else:
+            return render(request, "display.html", {"error": "Invalid OTP"})
+     
+def contact(request):
+    
+      if request.method == "POST":
+         name=request.POST['name'] 
+         email=request.POST['email']
+         subject=request.POST['subject']
+         message=request.POST['message']
+
+         if email:
+            send_mail(
+               subject="Thank you for contacting the page",
+               message=f""" 
+               Hi  {name} , this is an welcome message from manasa ,we'll get what your looking from me as soon as possible, thank you for contacting us.""",
+               from_email="manasays2001@gmail.com",
+               recipient_list=[email],
+               fail_silently=True
+            )
+            send_mail(
+               subject="New user enquiry",
+               message=f""" 
+               Hi  {name} , enquired about {subject} with the message {message} and the email is {email}.""",
+               from_email="shreyasms6@gmail.com",
                recipient_list=[email],
                fail_silently=True
             )
